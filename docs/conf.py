b14335de391e3a62030069120c93c8522474011f
Kwpolska <kwpolska@gmail.com>
2013-05-31 11:49:02 +0200

    fixed docs short version number
    Signed-off-by: Kwpolska <kwpolska@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'2012-2013, Kwpolska'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1'
+version = '0.2'
 # The full version, including alpha/beta/rc tags.
 release = '0.2.1'
 
